d871e52befec9a5960f191ac3eb64f23ff0e4759
Bhusan Dotel <bhusandotel1@gmail.com>
2025-11-09 21:26:17 +0545

    Feat: successfully provided the context to model

--- app/agent.py
+++ app/agent.py
@@ -1,54 +1,32 @@
 import json
 import os
-from pydantic_ai import Agent
-from pydantic_ai.models.google import GoogleModel
-from pydantic_ai.providers.google import GoogleProvider
 from dotenv import load_dotenv
-from app.schemas import IntentAndContainer
+from fastmcp import Client
+from google import genai
 
 load_dotenv()
 
 MODEL = "gemini-2.5-flash"
 API_KEY = os.getenv("GEMINI_API_KEY")
 
-provider = GoogleProvider(api_key=API_KEY)
-model = GoogleModel(MODEL, provider=provider)
-agent = Agent(model, output_type=IntentAndContainer)
+
+gemini_client = genai.Client()
+mcp_client = Client("mcp_client_Server\mcp_server.py")
 
 class ContainerAgent:
 
     async def process_query(self, query: str):
-            prompt = f"""
-        You are an intelligent agent that extracts structured data from container queries.
-
-        Given a query like:
-        "{query}"
-
-        Return a JSON with exactly two fields:
-        1. "intent": one of ["full_container_details", "check_availability", "get_location", "check_holds", "get_last_free_day"]
-        2. "container": the container number (e.g., "MSDU123456").
-
-        Example output:
-        {{
-        "intent": "full_container_details",
-        "container": "MSDU123456"
-        }}
-
-        Respond **ONLY** with the JSON, no explanations, nothing else.
-        """
-
-            response = await agent.run(prompt)
-
-            return response.output.model_dump()
-
-
-if __name__ == "__main__":
-    import asyncio
-
-    async def main():
-        container_agent = ContainerAgent()
-        query = "Where is container MSDU123456?"
-        result = await container_agent.process_query(query)
-        print(result)
-
-    asyncio.run(main())
+        try:           
+            async with mcp_client:
+                response = await gemini_client.aio.models.generate_content(
+                            model=MODEL,
+                            contents=query,
+                            config=genai.types.GenerateContentConfig(
+                                temperature=0,
+                                tools=[mcp_client.session], 
+                            ),
+                        )
+                return response.text
+        except Exception as e:
+            print("==============")
+            print(f"Error processing query: {e}")
--- app/schemas.py
+++ app/schemas.py
@@ -5,7 +5,3 @@ from typing import Optional
 class QueryRequest(BaseModel):
     query: str
 
-
-class IntentAndContainer(BaseModel):
-    intent: str
-    container: str
